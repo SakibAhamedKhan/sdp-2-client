15a25a300d36732e166de21694ed0e01ddecb316
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-05-17 01:56:06 +0600

    police dashboard banner added

--- src/Pages/Police/PoliceDashboard/PoliceDashboard.js
+++ src/Pages/Police/PoliceDashboard/PoliceDashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PoliceDashBanner from './PoliceDashBanner';
 import PoliceNavbar from './PoliceNavbar';
 
 const PoliceDashboard = () => {
 	return (
 		<div>
 			<PoliceNavbar></PoliceNavbar>
-			
+			<PoliceDashBanner></PoliceDashBanner>
 		</div>
 	);
 };
