467a6be9521a307fd36e95c992cb4da8c377e069
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-05-17 13:43:41 +0600

    police register form design done

--- src/Pages/Police/PoliceDashboard/PoliceDashBanner.js
+++ src/Pages/Police/PoliceDashboard/PoliceDashBanner.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import banner3 from '../../../asset/images/banner3.jpg';
 
 const PoliceDashBanner = () => {
+	const navigate = useNavigate();
 	return (
-		<div class="hero min-h-screen" style={{backgroundImage: "url(https://api.lorem.space/image/fashion?w=1000&h=800)"}}>
+		<div class="hero min-h-screen" style={{backgroundImage: `url(${banner3})`}}>
 			<div class="hero-overlay bg-opacity-60"></div>
 			<div class="hero-content text-center text-neutral-content">
 				<div class="max-w-md">
-					<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-					<p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-					<button class="btn btn-primary">Get Started</button>
+					<h1 class="mb-5 text-5xl font-bold text-center text-white">Welcome</h1>
+					<p class="mb-5 text-center text-white">Please Register your Police Information.</p>
+					<button onClick={ () => {
+						navigate('/policeRegister');
+					}} class="btn btn-primary text-white">Register</button>
 				</div>
 			</div>
 		</div>
--- src/Pages/Police/Register/Register.js
+++ src/Pages/Police/Register/Register.js
@@ -2,10 +2,54 @@ import React from 'react';
 import PoliceNavbar from '../PoliceDashboard/PoliceNavbar';
 
 const Register = () => {
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		const name = event.target.name.value;
+		const rangId =  event.target.rangId.value;
+		const responsibility = event.target.responsibility.value;
+		const thana = event.target.thana.value;
+		const phone = event.target.phone.value;
+		console.log(name, rangId, responsibility, thana, phone);
+	}
 	return (
 		<div>
 			<PoliceNavbar></PoliceNavbar>
-			<h2>Register</h2>
+			
+				<div class="card w-2/5 bg-white shadow-xl mx-auto my-10">
+					<h2 className='text-center text-primary text-2xl font-bold mt-5'>Register</h2>
+					<div className='mt-5 mb-7'>
+						<form className='grid grid-cols-1 gap-y-4 justify-items-center items-center' onSubmit={handleSubmit}>
+							<input name='name' type="text" placeholder="Your Name" class="input input-bordered w-full max-w-xs"  required/>
+							<input name='rangId' type="text" placeholder="Your Rang ID" class="input input-bordered w-full max-w-xs"  required/>
+							{/* <input type="text" placeholder="Responsibility" class="input input-bordered w-full max-w-xs" /> */}
+							<select name='responsibility' class="select select-bordered w-full max-w-xs text-slate-500" required>
+								<option value=''>Responsibility</option>
+								<option value='Inspector'>Inspector</option>
+								<option value='SI'>SI</option>
+								<option value='ASI'>ASI</option>
+								<option value='Constable'>Constable</option>
+							</select>
+							{/* <input type="text" placeholder="Thana" class="input input-bordered w-full max-w-xs"  required/> */}
+							<select name='thana' class="select select-bordered w-full max-w-xs text-slate-500" required>
+								<option value='' className='text-slate-300' >Thana</option>
+								<option value='Chandgaon'>Chandgaon</option>
+								<option value='Chandgaon'>Chandgaon</option>
+								<option value='Double Mooring'>Double Mooring</option>
+								<option value='Patenga'>Patenga</option>
+								<option value='Kotwali'>Kotwali</option>
+								<option value='Pahartali'>Pahartali</option>
+								<option value='Panchlaish'>Panchlaish</option>
+								<option value='Bakalia'>Bakalia</option>
+								<option value='Karnafuli'>Karnafuli</option>
+								<option value='Halishahar'>Halishahar</option>
+								<option value='Khulshi'>Khulshi</option>
+							</select>
+							<input name='phone' type="number" placeholder="Your Phone Number" class="input input-bordered w-full max-w-xs"  required/>
+							<input name='' type="submit" value="Submit" className='btn btn-primary text-white px-5'  required/>
+						</form>
+					</div>
+				</div>
 		</div>
 	);
 };
