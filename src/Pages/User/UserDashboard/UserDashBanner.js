d6e645be75cc94018693f401cba1e153216676af
Sakib Ahamed Khan <96885912+SakibAhamedKhan@users.noreply.github.com>
2022-06-02 01:24:50 +0600

    Some Align Updated

--- src/Pages/User/UserDashboard/UserDashBanner.js
+++ src/Pages/User/UserDashboard/UserDashBanner.js
@@ -53,9 +53,9 @@ const UserDashBanner = () => {
 					
 					<div class="form-control">
 						<div class="input-group">
-							<form onSubmit={handleSearch}>
-								<input  name='search' type="text" placeholder="Enter Police Range ID..." class="input input-bordered w-96 text-xl text-black" required />
-								<button type='submit' class="btn btn-square btn-primary text-white w-16">
+							<form onSubmit={handleSearch} className='flex items-center'>
+								<input  name='search' type="text" placeholder="Enter Police Range ID..." class="input input-bordered w-96 text-xl text-black h-12" required />
+								<button type='submit' class="btn btn-square btn-primary text-white w-16 p-0">
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
 								</button>
 							</form>
